86bfad4d9b9c28ac1d64a539790168417907798b
98bits <developer@98bits.com>
2025-12-25 05:13:43 -0500

    Create YearTag

---
+++ Sources/VaporWebKit/Tags/YearTag.swift
@@ -0,0 +1,36 @@
+//
+//  YearTag.swift
+//  vapor-webkit || Swift 6.0 || macOS 26.2
+//
+//  © 2025 98bits - https://98bits.com
+//
+
+import Foundation
+import Leaf
+
+enum YearTagError: Error {
+    case invalidFormatParameter
+    case tooManyParameters
+}
+
+public struct YearTag: LeafTag {
+    public init() {}
+
+    public func render(_ ctx: LeafContext) throws -> LeafData {
+        let formatter = DateFormatter()
+        switch ctx.parameters.count {
+        case 0: formatter.dateFormat = "yyyy"
+        case 1:
+            guard let string = ctx.parameters[0].string else {
+                throw YearTagError.invalidFormatParameter
+            }
+
+            formatter.dateFormat = string
+        default:
+            throw YearTagError.tooManyParameters
+        }
+
+        let dateAsString = formatter.string(from: Date())
+        return LeafData.string(dateAsString)
+    }
+}
