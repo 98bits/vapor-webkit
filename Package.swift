ff612d233be05944bdd9f8f26d202b076b4c7004
98bits <developer@98bits.com>
2025-12-25 05:12:05 -0500

    Update Package

--- Package.swift
+++ Package.swift
@@ -1,22 +1,28 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "VaporWebKit",
+    name: "vapor-webkit",
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VaporWebKit",
             targets: ["VaporWebKit"]
         ),
     ],
+    dependencies: [
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VaporWebKit"
+            name: "VaporWebKit",
+            dependencies: [
+                .product(name: "Leaf", package: "leaf"),
+            ],
         ),
         .testTarget(
             name: "VaporWebKitTests",
