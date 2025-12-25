ee347ca2973fa0e90382fba966d8ef8e1c121b2a
98bits <developer@98bits.com>
2025-12-25 05:12:27 -0500

    Update VaporWebKit

--- Sources/VaporWebKit/VaporWebKit.swift
+++ Sources/VaporWebKit/VaporWebKit.swift
@@ -1,2 +1,16 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  VaporWebKit.swift
+//  vapor-webkit || Swift 6.0 || macOS 26.2
+//
+//  © 2025 98bits - https://98bits.com
+//
+
+public struct VaporWebKitLibrary {
+    public private(set) var package = "VaporWebKit | vapor-webkit | https://vaporwebkit.com"
+
+    public init() {}
+
+    public func copyright() -> String {
+        return "© 2025 98bits.com"
+    }
+}
